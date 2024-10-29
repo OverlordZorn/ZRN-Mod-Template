2e2ed9a72b98a0adde36d16f984945d72d56bf85
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-10-30 00:04:08 +0100

    Update script_mod.hpp

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -2,8 +2,3 @@
 #define MAINPREFIX ~MAINPREFIX~
 
 #include "script_version.hpp"
-
-// #define VERSION MAJOR.MINOR.PATCH // current hemtt does not support a 3 parted number (example 0.1.1)
-#define VERSION MAJOR.MINOR
-#define VERSION_AR MAJOR,MINOR,PATCH
- 
