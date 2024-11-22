388c139785975702d2516c7865cf9c2b5c9f9d9e
OverlordZorn <dennisAzorn@gmail.com>
2024-11-22 13:17:16 +0100

    Update script_component.hpp

--- addons/template_addon/script_component.hpp
+++ addons/template_addon/script_component.hpp
@@ -1,5 +1,8 @@
 #define COMPONENT ~TEMPLATE_ADDON~
-#define COMPONENT_BEAUTIFIED ~TEMPLATE_ADDON~       // This is being used for the Addon's Name and can be "My Addon Template Framework"
+
+
+// This is being used for the Addon's Name and can be "My Addon Template Framework"
+#define COMPONENT_BEAUTIFIED ~TEMPLATE_ADDON~
 
 
 #include "\~MAINPREFIX~\~PREFIX~\addons\main\script_mod.hpp"
